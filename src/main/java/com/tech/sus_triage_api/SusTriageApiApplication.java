f75e0952a823a664f212dc1ad4c2050afc0db403
Leonardo Ferreira <leoventura245@gmail.com>
2026-02-05 17:50:22 -0300

    estruturando o projeto

---
+++ src/main/java/com/tech/sus_triage_api/SusTriageApiApplication.java
@@ -0,0 +1,13 @@
+package com.tech.sus_triage_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusTriageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusTriageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/sus_triage_api/SusTriageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.sus_triage_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusTriageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
