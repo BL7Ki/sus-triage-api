44e19b7ee7abdfd21f15bfc457e31f215919621f
Leonardo Ferreira <leoventura245@gmail.com>
2026-02-13 22:24:51 -0300

    adicionando cachemanager

--- src/main/java/com/tech/sus_triage_api/config/RedisConfig.java
+++ src/main/java/com/tech/sus_triage_api/config/RedisConfig.java
@@ -1,8 +1,11 @@
 package com.tech.sus_triage_api.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
@@ -17,5 +20,12 @@ public class RedisConfig {
                 .entryTtl(Duration.ofMinutes(10)) // Cache de unidades dura 10 min
                 .disableCachingNullValues()
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory, RedisCacheConfiguration cacheConfiguration) {
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
     }
 }
